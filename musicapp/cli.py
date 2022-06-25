0030cf8c4850630ef12fa0556f05ac853a7a0add
Brian Dennis <bmd+github@crossjam.net>
2022-06-24 22:33:49 -0400

    Add in logging, json output support

--- musicapp/cli.py
+++ musicapp/cli.py
@@ -1,24 +1,61 @@
+import json
+import logging
+import sys
+
 import click
 from tabulate import tabulate
 
 from .fabric import missing_fabric_content
+from .logconfig import DEFAULT_LOG_FORMAT, logging_config
+
+
+pass_dict = click.make_pass_decorator(dict)
 
 
 @click.group()
 @click.version_option()
-def cli():
+@click.option(
+    "--log-format",
+    type=click.STRING,
+    default=DEFAULT_LOG_FORMAT,
+    help="Python logging format string",
+)
+@click.option(
+    "--log-level", default="ERROR", help="Python logging level", show_default=True
+)
+@click.option(
+    "--log-file",
+    help="Python log output file",
+    type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+    default=None,
+)
+def cli(log_format, log_level, log_file):
     "Tools For Music.app Data"
+    logging_config(log_format, log_level, log_file)
 
 
 @cli.group("fabric")
-def fabric():
+@click.option(
+    "+live/-live", "fabriclive", help="Query FabricLive releases", default=False
+)
+def fabric(fabriclive):
     pass
 
 
 @fabric.command(name="missing")
-def missing_fabric():
-    output_table = tabulate(
-        missing_fabric_content().items(), headers=["Release", "Name"]
-    )
+@click.option("--fmt", "tablefmt", help="Table format", default="simple")
+def missing_fabric(tablefmt):
+    logging.debug("tablefmt: %s", tablefmt)
 
-    click.echo(output_table)
+    if tablefmt == "json":
+        rows = [{"release": k, "title": v} for k, v in missing_fabric_content().items()]
+        for row in rows:
+            json.dump(row, sys.stdout)
+            print()
+    else:
+        output_table = tabulate(
+            missing_fabric_content().items(),
+            headers=["Release", "Title"],
+            tablefmt=tablefmt,
+        )
+        click.echo(output_table)
